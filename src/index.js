77abf3d42bb6ae631b9503bf2d30cd9036160218
meuequalsD <nirmalkrishnav@gmail.com>
2020-08-20 23:22:29 +0530

    warning removed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Firebase, { FirebaseContext } from './services/firebase';
 
 
 ReactDOM.render(
